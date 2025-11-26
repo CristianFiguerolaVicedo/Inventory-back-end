c20822afd296635c534491ef833361da4fa6b873
CriFigShe <crifigshe@gmail.com>
2025-11-26 17:27:20 +0100

    Hot fix

--- src/main/java/com/cristian/inventory/controller/ProfileController.java
+++ src/main/java/com/cristian/inventory/controller/ProfileController.java
@@ -6,6 +6,7 @@ import com.cristian.inventory.service.ProfileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,5 +35,11 @@ public class ProfileController {
                     "message", e.getMessage()
             ));
         }
+    }
+
+    @GetMapping("/profile")
+    public ResponseEntity<ProfileDto> getPublicProfile() {
+        ProfileDto publicProfile = profileService.getPublicProfile(null);
+        return ResponseEntity.ok(publicProfile);
     }
 }
